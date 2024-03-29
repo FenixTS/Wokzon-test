0b98d7556ffd1cc4a662e76446dc142fe8ee7318
Fenix <fenixt.s2000@gmail.com>
2024-03-29 18:19:07 +0530

    app.use

--- app.js
+++ app.js
@@ -124,12 +124,13 @@ app.use('/api/v1/', cartItem);
 
 // // production script
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static("./client/build"));
 
 // Serve the React app for any other requests
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+
 
 
 
