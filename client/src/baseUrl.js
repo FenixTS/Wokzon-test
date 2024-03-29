d01d51225fcdb2d205c33a867c3a95a4a8c9c5e6
Fenix <fenixt.s2000@gmail.com>
2024-03-29 18:33:00 +0530

    base url

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://localhost:8000/api/v1'
-export const baseUrl = 'https://wokzon-web-app.azurewebsites.net/api/v1'
+export const baseUrl = "https://wokzon-web-app.azurewebsites.net"
 // 'http://localhost:3001';
